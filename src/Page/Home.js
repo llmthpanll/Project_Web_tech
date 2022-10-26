57c8745bba016acbc3ea81a3b35802a3d72fb654
RomeyKung <88416990+RomeyKung@users.noreply.github.com>
2022-10-26 15:44:19 +0700

    update

--- src/Page/Home.js
+++ src/Page/Home.js
@@ -4,11 +4,9 @@ const Home = ()=>{
         <div>
             <Navbar/>
             <img className="w-full" src={"https://images.unsplash.com/photo-1568605114967-8130f3a36994?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"}/>
-            <div>
-                <h1 className="text-4xl font-bold text-center">บ้านเดี่ยว</h1>
-            </div>
+           
         </div>
-        
+
     );
 }
 
