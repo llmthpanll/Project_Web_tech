5e4495a5e85640e159a3c612f3a0bce13f5f87c4
bosssxandaR <113000499+bosssxandaR@users.noreply.github.com>
2022-11-05 18:17:38 +0700

    update
    button on home page

--- src/Page/Home.js
+++ src/Page/Home.js
@@ -6,15 +6,18 @@ const Home = ()=>{
     return(
         <div>
             <Navbar/>
-            <div className="z-0">
+            <div className="z-0 ">
 
                 <div className="flex bg-head2 bg-cover h-screen text-center justify-center relative">
                     <div className="text-white absolute bottom-16 font-semibold text-6xl">
                         Welcome to Shop
                     </div>
+
                 </div>
-                <div className="bg-head bg-cover h-screen relative">
-                    <button className="font-semibold text-6xl absolute bottom-16">ปุ่มเอง</button>
+                <div className=" flex bg-head bg-cover h-screen relative text-center justify-center ">
+                   <button class=" font-Kanit absolute bottom-10 bg-gray-300 bg-opacity-75 hover:bg-gray-200 text-black font-bold py-2 px-4  rounded w-48 text-2xl">
+  จองเลย
+</button>
                 </div>
             <Footer/>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,9 @@ module.exports = {
         'head2': "url('https://images.unsplash.com/photo-1568605114967-8130f3a36994?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80')",
 
       }
+    ,  fontFamily: {
+             Kanit : ['Kanit']
+            }
   },
   },
   plugins: [],
