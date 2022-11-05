176bc60efc62b9a9617f3cef3568bec4c17830a8
KarnKaewprapa <88415650+KarnKaewprapa@users.noreply.github.com>
2022-11-05 15:14:36 +0700

    Update Contact.js

---
+++ src/Page/Contact.js
@@ -0,0 +1,11 @@
+import Navbar from "../component/Navbar"
+
+const Contact =()=>{
+    return(
+        <div>
+            <div className="container">
+                <h1 className="text-center">Contact</h1>
+            </div>
+        </div>
+    )
+}
