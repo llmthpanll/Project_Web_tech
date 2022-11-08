85b17ede3719da42c9f844ce9c9c3e6d01fc1bb5
KarnKaewprapa <88415650+KarnKaewprapa@users.noreply.github.com>
2022-11-09 03:26:00 +0700

    update contact

---
+++ src/Page/Towerhouse.js
@@ -0,0 +1,15 @@
+import Navbar from "../component/Navbar"
+import SlideShow from "../component/Slideshow"
+const Towerhouse = ()=>{
+    return(
+        <div>
+            <Navbar/>
+            {/* <SlideShow/> */}
+
+
+        </div>
+
+    );
+}
+
+export default Towerhouse;
