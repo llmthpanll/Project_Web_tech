0c66f463a3ac0f6ff4ecb4e3bf2b7bdfef7460f0
RomeyKung <88416990+RomeyKung@users.noreply.github.com>
2022-11-10 14:16:34 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,7 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'head': "url('../src/Page/Pic/1.jpg')",
-        'head2': "url('https://images.unsplash.com/photo-1568605114967-8130f3a36994?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80')",
-        'house':"url('../src/Page/Pic/house/นาราสิริ บางนา/1-01.png')"
+
       }
     ,  fontFamily: {
              Kanit : ['Kanit']
