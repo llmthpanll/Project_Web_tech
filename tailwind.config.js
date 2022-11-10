429819674d9682e62d9f95956e25a91ca621eebc
RomeyKung <88416990+RomeyKung@users.noreply.github.com>
2022-11-10 14:20:51 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
-/** @type {import('tailwindcss').Config} */
+/ @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src//*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
+        'head1': "url('../src/Page/Pic/1.jpg')",
+        'head2': "url('../src/Page/Pic/house/สราญสิริ บางนา/1-01.png')",
+        'head3': "url('../src/Page/Pic/condo/เอ็กซ์ที ห้วยขวาง/1-01.png')",
+        'head4': "url('../src/Page/Pic/1.jpg')",
+        'head5': "url('../src/Page/Pic/1.jpg')",
 
       }
     ,  fontFamily: {
