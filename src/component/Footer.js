ed9a18088501eea14b95e8b60834d89bae053d9e
RomeyKung <88416990+RomeyKung@users.noreply.github.com>
2022-11-10 12:35:48 +0700

    fix footer
    fix position

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -3,7 +3,7 @@ function Footer() {
 
   return (
     // <div className="fixed top-0 left-0 right-0 z-10"></div>
-    <div className="fixed bottom-0 left-0 right-0 justify-items-center text-center font-medium bg-gray-200">
+    <div className="bottom-0 left-0 right-0 justify-items-center text-center font-medium bg-gray-200">
         <ul className="grid justify-items-center ">
             <div className="flex p-3">
             <Link to="/Company"><li className="px-3">Company</li></Link>
