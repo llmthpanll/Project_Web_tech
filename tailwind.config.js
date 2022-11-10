163f05a775ee0c5f982cb45fe562b5b7a823b8b2
bosssxandaR <113000499+bosssxandaR@users.noreply.github.com>
2022-11-10 12:50:29 +0700

    Update tailwind.config.js
    fix rome

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
@@ -9,7 +8,6 @@ module.exports = {
       backgroundImage: {
         'head': "url('../src/Page/Pic/1.jpg')",
         'head2': "url('https://images.unsplash.com/photo-1568605114967-8130f3a36994?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80')",
-        
 
       }
     ,  fontFamily: {
