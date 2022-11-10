e7c67e21b1ab91ccd909b48788dcc41d01ae2497
RomeyKung <88416990+RomeyKung@users.noreply.github.com>
2022-11-10 14:19:53 +0700

    Update Home.js

--- src/Page/Home.js
+++ src/Page/Home.js
@@ -1,30 +1,47 @@
 import Navbar from "../component/Navbar"
 import Footer from "../component/Footer"
 import Slidehome from "../component/Slidehome"
-
-const Home = () => {
-    return (
+const Home = ()=>{
+    return(
         <div>
-            <Navbar />
+            <Navbar/>
             <div className="z-0 ">
 
-                <div className="flex bg-head2 bg-cover h-screen text-center justify-center relative">
-                    <div className="text-white absolute bottom-16 font-semibold text-6xl">
-                        Welcome to Shop
+                <div className="flex bg-head1 bg-cover h-screen text-center justify-center relative">
+                    <div className=" font-Kanit text-white absolute bottom-44  text-6xl">
+                    Head TEXT EX. Home for Sale!!!
+
+
+                    </div>
+                    <div className=" font-Kanit text-white absolute bottom-32  text-5xl">
+                    Infomation TEXT EX. Bla bla bla bla bla
+
+
+
                     </div>
 
-                </div>
-                <div className=" flex bg-head bg-cover h-screen relative text-center justify-center ">
-                        <button className=" font-Kanit absolute bottom-10 bg-gray-300 bg-opacity-75 hover:bg-gray-200 text-black font-bold py-2 px-4  rounded w-48 text-2xl">จองเลย</button>
-                </div>
-                <div className=" flex bg-house bg-cover h-screen relative text-center justify-center ">
-                </div>
-                    {/* <Slidehome/> */}
-                    <Footer />
-                </div>
-            </div>
+                    <button class=" font-Kanit absolute bottom-10 bg-gray-300 bg-opacity-75 hover:bg-gray-200 text-black font-bold py-2 px-4  rounded w-48 text-2xl">
+  จองเลย
+</button>
 
-            );
+                </div>
+                <div className=" flex bg-head2 bg-cover h-screen relative text-center justify-center ">
+                   <button class=" font-Kanit absolute bottom-10 bg-gray-300 bg-opacity-75 hover:bg-gray-200 text-black font-bold py-2 px-4  rounded w-48 text-2xl">
+  จองเลย
+</button>
+                </div>
+                <div className=" flex bg-head3 bg-cover h-screen relative text-center justify-center ">
+                   <button class=" font-Kanit absolute bottom-10 bg-gray-300 bg-opacity-75 hover:bg-gray-200 text-black font-bold py-2 px-4  rounded w-48 text-2xl">
+  จองเลย
+</button>
+                </div>
+
+            <Footer/>
+            </div>
+            {/* <Slidehome/> */}
+        </div>
+
+    );
 }
 
-            export default Home;
+export default Home;
