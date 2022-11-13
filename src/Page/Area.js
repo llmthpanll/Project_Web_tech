77e3229c7f9d594a4d873d8091b1f713f33cae6a
bosssxandaR <113000499+bosssxandaR@users.noreply.github.com>
2022-11-13 19:56:26 +0700

    json + all page

--- src/Page/Area.js
+++ src/Page/Area.js
@@ -13,17 +13,29 @@ const Condo = () => {
                         <div class="max-w-sm rounded overflow-hidden shadow-lg">
                             <img class="w-full" src={element.path + "/1-01.png"} alt="home" />
                             <div class="px-6 py-4 bg-white">
-                                <div class="font-bold text-xl mb-2">{element.name}</div>
-                                <p class="text-gray-700 text-base">
-                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+                            <div class="font-prompt font-bold text-xl mb-2">{element.name}</div>
+                                <span class="inline font-prompt text-gray-700 text-sm">
+                                {element.description}
+                                </span>
+                                <div class="font-prompt font-bold text-lg mb-2">{element.price}</div>
+                                <img class="w-6 py-2" src={element.icon1}  />
+                                <p class=" font-prompt text-gray-700 text-sm">
+                                {element.location1}
                                 </p>
+                                <img class="w-6 py-2" src={element.icon2}  />
+                                <p class=" font-prompt text-gray-700 text-sm">
+                                {element.location2}
+                                </p>
+                            
                                 <button class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
                                     Button
                                 </button>
+                            
+
                             </div>
+
                         </div>
                     </div>
-
                 ))}
 
             </div>
--- src/Page/House.js
+++ src/Page/House.js
@@ -2,9 +2,7 @@ import Navbar from "../component/Navbar"
 import Footer from "../component/Footer"
 import pic from "../path/home.json";
 import React, { useState } from "react"
-
-console.log(pic.house)
-const House = () => {
+const Condo = () => {
     const [sell, setSell] = useState(pic.house)
     return (
         <div className="bg-[url('https://media.istockphoto.com/id/92893866/photo/space-stars-texture.jpg?b=1&s=170667a&w=0&k=20&c=XI0UEMpgVsp2BLl_FmKvHDDtaW7LWnH008h2yCFqrgQ=')]">
@@ -12,19 +10,27 @@ const House = () => {
                 <Navbar />
                 {sell.map((element, index) => (
                     <div>
-                        <p></p>
                         <div class="max-w-sm rounded overflow-hidden shadow-lg">
                             <img class="w-full" src={element.path + "/1-01.png"} alt="home" />
                             <div class="px-6 py-4 bg-white">
-                                <div class="font-bold text-xl mb-2">{element.name}</div>
-                                <p class="text-gray-700 text-base">
-                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+                            <div class="font-prompt font-bold text-xl mb-2">{element.name}</div>
+                                <span class="inline font-prompt text-gray-700 text-sm">
+                                {element.description}
+                                </span>
+                                <div class="font-prompt font-bold text-lg mb-2">{element.price}</div>
+                                <img class="w-6 py-2" src={element.icon1}  />
+                                <p class=" font-prompt text-gray-700 text-sm">
+                                {element.location1}
                                 </p>
-    
+                                <img class="w-6 py-2" src={element.icon2}  />
+                                <p class=" font-prompt text-gray-700 text-sm">
+                                {element.location2}
+                                </p>
+                            
                                 <button class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
                                     Button
                                 </button>
-
+                            
 
                             </div>
 
@@ -38,4 +44,4 @@ const House = () => {
     );
 }
 
-export default House;
+export default Condo;
