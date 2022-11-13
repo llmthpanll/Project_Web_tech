9e02cbcf63ed3a85a01d64b21ec94908d083244d
Methasit Prateepmaethakul <54169574+llmthpanll@users.noreply.github.com>
2022-11-13 18:49:11 +0700

    Update Footer.js

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -3,43 +3,44 @@ import logo from "../Page/Pic/logo.png";
 // function Footer() {
 const Footer = () => {
   return (
-    <div>
-      <div className="grid grid-cols-3   font-medium bg-green-blue">
-        <div className="flex">
+    <div className="bg-black text-white p-5">
+      {/* <div className="grid grid-cols-3 font-medium "> */}
+        {/* <div className="text-center flex flex-col items-center justify-center">
+          <p>สมาชิกกลุ่ม</p>
+          <ul className="text-left list-disc grid gap-x-8">
+            <li>พิชญุตม์ ศรีม่วง 64070074</li>
+            <li>กัมปนาท คนคล่อง 64070125</li>
+            <li>กานต์ แก้วประภา 64070126</li>
+            <li>ภาคิน เค้าภูไทย 64070207</li>
+            <li>เมธาสิทธิ์ ประทีปเมธากุล 64070215</li>
+          </ul>
+        </div> */}
+
+        {/* <div className="flex justify-center items-center">
           <img
             src={logo}
             alt="Logo"
             border="0"
             className="flex w-20 h-20 m-2"
           ></img>
-        </div>
+        </div> */}
 
-        <div className="text-center flex flex-col items-center justify-center">
-          <ul className="text-left list-disc grid grid-cols-2 gap-x-8">
+        {/* <div className="text-center flex flex-col items-center justify-center"> */}
+          {/* <ul className="text-left list-disc grid grid-cols-2 gap-x-8">
             <li>บ้านเดี่ยว</li>
             <li>ทาวน์เฮาส์</li>
             <li>คอนโด</li>
             <li>พิ้นที่ค้าขาย</li>
-          </ul>
-          <ul className="grid justify-items-center ">
+          </ul> */}
+          {/* <ul className="grid justify-items-center ">
             <Link to="/Contact">
               <li className="px-3">Contact</li>
             </Link>
-          </ul>
-        </div>
-
-        <div className="text-center flex flex-col items-center justify-center">
-          <ul className="text-left list-disc grid grid-cols-2 gap-x-8">
-            <li>เกี่ยวกับเรา</li>
-            <li>เงื่อนไขการใช้งาน</li>
-            <li>นโยบายความเป็นส่วนตัว</li>
-            <li>คำถามที่พบบ่อย</li>
-          </ul>
-
-        </div>
-      </div>
-      <div className="text-center">
-        <p>COPYRIGHT © 2019 , SANSIRI PUBLIC CO.,LTD ALL RIGHTS RESERVED.</p>
+          </ul> */}
+        {/* </div> */}
+      {/* </div> */}
+      <div className="text-center text-xs">
+        <p>COPYRIGHT © 2022</p>
       </div>
     </div>
   );
