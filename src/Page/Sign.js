d183ea2f777b90c5c73f0b367c59cf51a7976f38
KarnKaewprapa <88415650+KarnKaewprapa@users.noreply.github.com>
2022-11-13 17:31:55 +0700

    Update Sign.js

--- src/Page/Sign.js
+++ src/Page/Sign.js
@@ -1,7 +1,25 @@
 import Navbar from "../component/Navbar"
 import loginImg from "./Pic/condo/เอ็กซ์ที พญาไท/3-01.png"
 import { Outlet, Link } from "react-router-dom";
+import { useState } from "react";
+import { Alert, alertClasses } from "@mui/material";
+import Home from "./Home";
 const Sign = ()=>{
+    const road = '';
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        if (email.length == 0 || password.length == 0){
+            alert("if you dont have account you can log in as guest");
+        }else{
+            // location.href = "http://localhost:3000/Home";
+            alert("go to home");
+        }
+    }
+
+    console.log('render all', email.length, password.length);
     return(
         <div>
 
@@ -9,7 +27,7 @@ const Sign = ()=>{
                 <Navbar/>
             </div>
 
-            <div>
+            <form onSubmit={onSubmit}>
                 <div className="grid grid-cols-1 sm:grid-cols-2 h-screen w-full">
                     <div className="hidden sm:block">
                         <img className="h-full w-full object-cover" src={loginImg} alt=""/>
@@ -20,26 +38,38 @@ const Sign = ()=>{
                             <h2 className="text-4xl dark:text-white font-bold text-center">Sign in</h2>
                             <div className="flex flex-col text-gray-400 py-2">
                                 <label>User Name</label>
-                                <input className="rounded-lg bg-gray-700 mt-2 p-2 focus:border-blue-500 focus:bg-gray-800 focus:outline-none" type="text"/>
+                                <input
+                                id="email"
+                                className="rounded-lg bg-gray-700 mt-2 p-2 focus:border-blue-500 focus:bg-gray-800 focus:outline-none"
+                                type="email"
+                                aria-describedby="emailHelp"
+                                onChange={(e) => setEmail(e.target.value)}
+                                />
                             </div>
                             <div className="flex flex-col text-gray-400 py-2">
                                 <label>Password</label>
-                                <input className="rounded-lg bg-gray-700 mt-2 p-2 focus:border-blue-500 focus:bg-gray-800 focus:outline-none" type="password"/>
+                                <input
+                                id="password"
+                                className="rounded-lg bg-gray-700 mt-2 p-2 focus:border-blue-500 focus:bg-gray-800 focus:outline-none"
+                                type="password"
+                                aria-describedby="passwordHelp"
+                                onChange={(e) => setPassword(e.target.value)}
+                                />
                             </div>
                             <div className="flex justify-between text-gray-400 py-2">
                                 <p className="flex items-center"><input className="mr-2" type="checkbox"/> Remember Me</p>
                                 <p>forgot password</p>
                             </div>
-                            <Link to= "/Home">
-                                <button  className="w-full my-5 py-2 bg-teal-500 shadow-lg shadow-teal-500/50 hover:shadow-teal-500/40 text-white font-semiblod rounded-lg">Sign In</button>
-                            </Link>
-                            <Link to= "/Home">
+                            {/* <Link to= {road}> */}
+                                <button type="submit" className="w-full my-5 py-2 bg-teal-500 shadow-lg shadow-teal-500/50 hover:shadow-teal-500/40 text-white font-semiblod rounded-lg">Sign In</button>
+                            {/* </Link> */}
+                            <Link to= "/home">
                                 <button  className="w-full my-5 py-2 bg-teal-400 shadow-lg shadow-teal-500/50 hover:shadow-teal-500/40 text-white font-semiblod rounded-lg">Login as guest</button>
                             </Link>
                         </div>
                     </div>
                 </div>
-            </div>
+            </form>
 
         </div>
     )
