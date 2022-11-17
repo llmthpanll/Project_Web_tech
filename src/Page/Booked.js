116db2a977c0fd3120443f8fe3dc27b120ce1f8e
KarnKaewprapa <88415650+KarnKaewprapa@users.noreply.github.com>
2022-11-17 21:33:00 +0700

    Update Booked.js

--- src/Page/Booked.js
+++ src/Page/Booked.js
@@ -1,13 +1,18 @@
 import Navbar from "../component/Navbar"
 import Footer from "../component/Footer"
 import React, { useState } from "react"
-const Booked = (data) => {
-    
-    return (
-        <div className="">
-            <Navbar />
 
-            <Footer />
+const Booked = (data) => {
+
+    return (
+        <div>
+            <Navbar/>
+
+            <div className="text-center">
+                <div className="text-2xl text-black">Booking</div>
+
+            </div>
+            <Footer/>
         </div>
     );
 }
