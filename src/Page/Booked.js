dc3b10b6516a325d3d8b00ceed7b8c60b4473085
Methasit Prateepmaethakul <54169574+llmthpanll@users.noreply.github.com>
2022-11-18 02:38:08 +0700

    จัดให้กลางแล้วนะครับ กานต์

--- src/Page/Booked.js
+++ src/Page/Booked.js
@@ -6,10 +6,12 @@ const Booked = (data) => {
   return (
     <div>
         <Navbar />
-        <div className="mt-16 bg-cover h-screen text-center justify-center relative">
+        <div className="flex flex-col mt-16 mx-0 bg-cover h-screen text-center relative">
             <div className="text-black text-5xl">Booking</div>
-
-            <div className="justify-center w-60 h-60 border-10"></div>
+            <div className="text-black text-2xl">Your Booking</div>
+            <div className="border-2 border-red-600 mx-40">
+              <div>gear</div>
+            </div>
         </div>
         <Footer />
     </div>
