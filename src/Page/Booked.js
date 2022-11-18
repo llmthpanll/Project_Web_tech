8186ba9a6b30d2efcb45df4d50d00165ec690288
RomeyKung <88416990+RomeyKung@users.noreply.github.com>
2022-11-18 14:58:13 +0700

    Update Booked.js

--- src/Page/Booked.js
+++ src/Page/Booked.js
@@ -5,16 +5,16 @@ import Slideshow from "../component/Slideshow"
 
 const Booked = (home) => {
   const booked = home.data
-  const style_card = "p-14 grid grid-cols-2 items-center justify-center gap-12 box-border"
-  const style_head = "pt-20 text-center text-6xl"
+  const style_card = " p-14 grid grid-cols-2 items-center justify-center gap-12 box-border"
+  const style_head = " text-center text-4xl font-prompt font-medium"
   const getDatafromSlideShow = (data) => {
     home.deleteDataApp(data)
   }
   return (
     <div>
       <Navbar />
-      <h1 className={style_head}>บ้านเดี่ยวโดนใจ</h1>
-      <h1 className={style_head}>สไตล์ที่ใช่...ของท่านเปา</h1>
+      <h1 className={"pt-20"+style_head}>บ้านเดี่ยวโดนใจ</h1>
+      <h1 className={"pt-10 pb-10" + style_head}>สไตล์ที่ใช่...ของท่านเปา</h1>
       <div className={style_card}>
         {booked.map((element, index) => (
           <Slideshow key={element.name} prop={element} getdata={getDatafromSlideShow} />
